3440e2127917c4504814cb3497a4d88777e67542
andylamax <andylamax@programmer.net>
2023-04-21 18:06:19 +0300

    Added lexi as a part of the published flock

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,12 @@
+import com.vanniktech.maven.publish.MavenPublishBaseExtension
+import com.vanniktech.maven.publish.SonatypeHost
+
 @Suppress("DSL_SCOPE_VIOLATION") plugins {
     alias(androidx.plugins.library) apply false
     alias(kotlinz.plugins.multiplatform) apply false
     alias(kotlinz.plugins.serialization) apply false
-    alias(kotlinz.plugins.compose) apply false
-    alias(asoft.plugins.library) apply false
-    alias(petuska.plugins.root.npm.publish) apply false
+    alias(vanniktech.plugins.maven.publish) apply false
     alias(kotlinz.plugins.dokka)
-    alias(nexus.plugins.publish)
-    alias(asoft.plugins.deploy)
 }
 
 repositories {
@@ -16,6 +15,42 @@ repositories {
 
 subprojects {
     apply(plugin = "org.jetbrains.dokka")
+    apply(plugin = "com.vanniktech.maven.publish")
+
+    val p = this
+
+    configure<MavenPublishBaseExtension> {
+        publishToMavenCentral(SonatypeHost.DEFAULT,automaticRelease = true)
+
+        signAllPublications()
+
+        coordinates("tz.co.asoft", p.name, v)
+
+        pom {
+            name.set(p.name)
+            description.set(p.description)
+            inceptionYear.set("2019")
+            url.set("https://github.com/aSoft-Ltd/lexi")
+            licenses {
+                license {
+                    name.set("MIT License")
+                    url.set("https://github.com/aSoft-Ltd/lexi/blob/master/LICENSE")
+                }
+            }
+            developers {
+                developer {
+                    id.set("andylamax")
+                    name.set("Anderson Lameck")
+                    url.set("https://github.com/andylamax/")
+                }
+            }
+            scm {
+                url.set("https://github.com/aSoft-Ltd/lexi/")
+                connection.set("scm:git:git://github.com/aSoft-Ltd/lexi.git")
+                developerConnection.set("scm:git:ssh://git@github.com/aSoft-Ltd/lexi.git")
+            }
+        }
+    }
 }
 
 tasks.dokkaHtmlMultiModule {
