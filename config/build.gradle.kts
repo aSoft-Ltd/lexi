71b07e2512b0c382ef6093fc21a355b4bef2ad73
andylamax <andylamax@programmer.net>
2025-09-29 10:45:58 +0300

    Finished migrating symphony

--- config/build.gradle.kts
+++ config/build.gradle.kts
@@ -8,7 +8,7 @@ description = "A kotlin multiplatform solution to logging on the console"
 
 kotlin {
     if (Targeting.JVM) jvm { library() }
-    if (Targeting.JS) js(IR) { library() }
+    if (Targeting.JS) js(IR) { library() } // untill https://youtrack.jetbrains.com/issue/KT-80014 gets fixed // untill https://youtrack.jetbrains.com/issue/KT-80014 gets fixed
     if (Targeting.WASM) wasmJs { library() }
     if (Targeting.OSX) macOsTargets() else listOf()
 //    if (Targeting.NDK) ndkTargets() else listOf()
